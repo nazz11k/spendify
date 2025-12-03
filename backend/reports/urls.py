587136a47645a49bfbe3b6cf6a54cef934cbdd45
Rybchyskyi Nazarii <72073004+nazz11k@users.noreply.github.com>
2025-12-04 01:17:41 +0200

    Revert "Release v1.0.0"

--- backend/reports/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ActivityReportView, CategorySummaryView, OverTimeReportView
-
-urlpatterns = [
-    path('activity/', ActivityReportView.as_view(), name='activity-report'),
-    path('by-category/', CategorySummaryView.as_view(), name='category-report'),
-    path('over-time/', OverTimeReportView.as_view(), name='time-report'),
-]
--- backend/transactions/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Transaction, Category
-
-admin.site.register(Transaction)
-admin.site.register(Category)
